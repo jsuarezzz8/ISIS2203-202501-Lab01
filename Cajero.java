81e00fd6308d5dad37792f8cb41e24f180447229
Johan <jc.suarezs12@uniandes.edu.co>
2025-01-27 18:28:12 -0500

    Finalización del Lab

--- Cajero.java
+++ Cajero.java
@@ -2,25 +2,44 @@ public class Cajero extends Thread{
 
     private int id;
 
-    private int[][] filaClientes;
+    private int[] filaClientes;
 
     private double factorDeCansancio;
 
-    public Cajero(int id){
+    public Cajero(int id, int[] filaClientes){
         this.id = id;
+        this.filaClientes = filaClientes;
+        this.factorDeCansancio = 1.0;
     }
 
     public void run(){
 
-        for(int i = 1; i < 11; i++){
-            System.out.println("Cajero con id "+ id + " contando en " + i);
+        for(int i = 1; i < filaClientes.length+1; i++){
+            System.out.println("El cajero "+ id + " comienza a procesar la fila " + i);
+            recepcionCliente(i, filaClientes[i-1]);
+            try {
+                Thread.sleep((int) tiempoCalculado(filaClientes[i-1], factorDeCansancio));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            cansancioAcumulado(filaClientes[i-1]);
         }
+        System.out.println("El cajero "+ id + " ha terminado de atender");
 
     }
 
 
     public double tiempoCalculado(int tiempoBase, double factorDeCansancio){
         return tiempoBase * factorDeCansancio;
+    }
+
+    public void recepcionCliente(int cliente, int tiempo){
+        int tiempoCalculado = (int) tiempoCalculado(tiempo, factorDeCansancio);
+        System.out.println("El cajero " + id + " atendiendo al cliente " + cliente + " de la fila " + id + " (tiempo de procesammiento básico: " + tiempo + "ms) factor de cansancio actual: " + factorDeCansancio + " tiempo de procesamiento actual: " + tiempoCalculado + "ms");
+    }
+
+    public void cansancioAcumulado(int tiempoBase){
+        this.factorDeCansancio = this.factorDeCansancio + (tiempoBase * 0.001);
     }
     
 
--- Main.java
+++ Main.java
@@ -1,24 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static int[][] matriz;
 
     public static void main(String[] args) throws InterruptedException {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la ruta del archivo: ");
+        String ruta = sc.nextLine();
+        sc.close();
 
+        int cajeros = 0;
+        int clientes = 0;
 
-        Cajero cajero1 = new Cajero(1);
-        Cajero cajero2 = new Cajero(2);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(ruta));
+            String linea = br.readLine();
+            String datos[] = linea.split(";");
+            cajeros = Integer.parseInt(datos[0]);
+            clientes = Integer.parseInt(datos[1]);
+            matriz = new int[cajeros][clientes];
+            String linea1 = br.readLine();
+            while (linea1 != null) {
+                String datos1[] = linea1.split(";");
+                for (int i = Integer.parseInt(datos1[0]) - 1 ; i < Integer.parseInt(datos1[0]); i++) {
+                    for (int j = 0; j < datos1.length - 1; j++) {
+                        matriz[i][j] = Integer.parseInt(datos1[j+1]);
+                    }
+                }
+                linea1 = br.readLine();
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo");
+        }
 
-        cajero1.start();
-        Thread.sleep(10000);
-        cajero2.start();
-        // Leer el archivo .csv
+        for(int i = 1; i < cajeros+1; i++){
+            Cajero cajero = new Cajero(i, matriz[i-1]);
+            cajero.start();
+        }
 
-        // Crear la matriz
-
-        // Crear y empezar los threads
     }
 
-    public void leerArchivo(String ruta){
-
-    }
     
 }
+
+/*
+ * Respuesta:
+ * 
+ * El tercer ejemplo por lo que veo, el comportamiento que tiene es que los cajeros (threads) empiezan y terminan al mismo tiempo.
+ * Esto se debe a que la suma de los tiempos de los clientes de cada uno de los cajeros, siempre es la misma.
+ * Es por eso que los cajeros se demoran el mismo tiempo en atender a todos los clientes, terminando al mismo tiempo.
+ * 
+ */
